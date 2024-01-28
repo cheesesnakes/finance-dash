f5402c28b1a440c83f5b0feabd620561cc3bf16f
Shawn Dsouza <shawn27.dsouza@pm.me>
2024-01-28 12:14:13 +0530

    wrap layout in function

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import dash_html_components as html
 from logic import networth
 from logic import available
 from logic import budget
-from logic import accounts
 from figs import budget_fig
 from dash import dash_table
 from figs import accounts_fig
@@ -20,220 +19,226 @@ from figs import expense_fig
 from logic import last_review_date
 from logic import reimbursement
 from logic import current_debt
+from data import get_data
+from dash import Input, Output
 
 # Initialize the app
 app = dash.Dash(__name__)
 app.title = "Shawn's Financial Dashboard"
 
 # Define the app layout
-app.layout = html.Div(style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}, children=[
-    html.H1("Shawn's Financial Dashboard"),
-    
-    html.Div(id='summary', style={'display':'flex-wrap', 'margin':'auto', 'max_width':'500px'}, children=[
+def serve_layout():
+    layout = html.Div(style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}, children=[
+        html.H1("Shawn's Financial Dashboard"),
         
-        html.H2('Summary'),
-
-        html.Div(id='summary-2', style={'display':'flex'},children=[
-
-
-            html.Div(id='review-date',  style={'margin': '30px'}, children=[
+        html.Div(id='summary', style={'display':'flex-wrap', 'margin':'auto', 'max_width':'500px'}, children=[
             
-            html.H3('Last Review Date'),
-            
-            last_review_date.strftime('%d-%m-%Y, %A')
+            html.H2('Summary'),
 
-            # Add a div for diplaying networth here
+            html.Div(id='summary-2', style={'display':'flex'},children=[
 
-        ]),
-        
-        html.Div(id='networth',  style={'margin': '30px'}, children=[
-            
-            html.H3('Net Worth'),
-            
-            round(networth)
-            # Add a div for diplaying networth here
 
-        ]), 
-        
-            html.Div(id='available', style={'margin': '30px'}, children=[
+                html.Div(id='review-date',  style={'margin': '30px'}, children=[
+                
+                html.H3('Last Review Date'),
+                
+                last_review_date.strftime('%d-%m-%Y, %A')
+
+                # Add a div for diplaying networth here
+
+            ]),
             
-            html.H3('Available'),
+            html.Div(id='networth',  style={'margin': '30px'}, children=[
+                
+                html.H3('Net Worth'),
+                
+                round(networth)
+                # Add a div for diplaying networth here
+
+            ]), 
             
-            round(available)
+                html.Div(id='available', style={'margin': '30px'}, children=[
+                
+                html.H3('Available'),
+                
+                round(available)
+                # Add a div for available here
+
+            ]),
+
+            html.Div(id='reimbursement', style={'margin': '30px'}, children=[
+                
+                html.H3('Reimbursement'),
+
+                round(reimbursement)
+            ]),
+
             # Add a div for available here
 
+                html.Div(id='investment', style={'margin': '30px'}, children=[
+                
+                html.H3('Investment'),
+
+                "TBD"
+            ]),
+
+            # Add a div for investment here
+
+
+            ]),
+            
         ]),
 
-        html.Div(id='reimbursement', style={'margin': '30px'}, children=[
+        html.Div(id='balance', children=[
             
-            html.H3('Reimbursement'),
+            html.H2('Balance'),
+            # Add more components for section 2 here
 
-            round(reimbursement)
+            # adding a flexbox for the balance section
+
+            html.Div(id='balance-2', style={'display':'flex'},children=[
+
+                html.Div(id='trend-graph', style={'margin': '10px'}, children=[
+
+                    dcc.Graph(figure=earn_fig)
+                
+                ]),
+
+                html.Div(id='balance-graph', style={'margin': '10px'}, children=[
+
+                    dcc.Graph(figure=accounts_fig)
+                ]),
+        ]),
+        
         ]),
 
-        # Add a div for available here
-
-            html.Div(id='investment', style={'margin': '30px'}, children=[
+        html.Div(id='budget', children=[
             
-            html.H3('Investment'),
+            html.H2('Budget'),
+            
+            html.Div(id='budget-2', style={'display':'flex'},children=[
+                
+                html.Div(id='budget-table-div', style={'margin': '10px'}, children=[
 
-            "TBD"
+                    dash_table.DataTable(
+
+                        id='budget-table',
+                        columns=[{"name": i, "id": i} for i in budget[['budget_head', 'goal', 'budgetted', 'debit', 'mean']].columns],
+                        data=budget[['budget_head', 'goal', 'budgetted', 'debit', 'mean']].to_dict('records'),
+                    )
+
+                
+                ]),
+
+                html.Div(id='budget-graph', style={'margin': '10px'}, children=[
+
+                    dcc.Graph(figure=budget_fig)
+                ]),
         ]),
-
-        # Add a div for investment here
-
-
-        ]),
-        
-    ]),
-
-    html.Div(id='balance', children=[
-        
-        html.H2('Balance'),
-        # Add more components for section 2 here
-
-        # adding a flexbox for the balance section
-
-        html.Div(id='balance-2', style={'display':'flex'},children=[
-
-            html.Div(id='trend-graph', style={'margin': '10px'}, children=[
-
-                dcc.Graph(figure=earn_fig)
-            
-            ]),
-
-            html.Div(id='balance-graph', style={'margin': '10px'}, children=[
-
-                dcc.Graph(figure=accounts_fig)
-            ]),
-    ]),
-    
-    ]),
-
-    html.Div(id='budget', children=[
-        
-        html.H2('Budget'),
-        
-         html.Div(id='budget-2', style={'display':'flex'},children=[
-             
-            html.Div(id='budget-table-div', style={'margin': '10px'}, children=[
-
-                dash_table.DataTable(
-
-                    id='budget-table',
-                    columns=[{"name": i, "id": i} for i in budget[['budget_head', 'goal', 'budgetted', 'debit', 'mean']].columns],
-                    data=budget[['budget_head', 'goal', 'budgetted', 'debit', 'mean']].to_dict('records'),
-                )
-
-            
-            ]),
-
-            html.Div(id='budget-graph', style={'margin': '10px'}, children=[
-
-                dcc.Graph(figure=budget_fig)
-            ]),
-    ]),
-
-    ]),
-
-    html.Div(id='Income', children=[
-        
-        html.H2('Income Summary'),
-        # Add more components for section 4 here
-
-        html.Div(id='income-2', style={'display':'flex'},children=[
-             
-            html.Div(id='income-table-div', style={'margin': '10px'}, children=[
-
-                dash_table.DataTable(
-
-                    id='income-table',
-                    columns=[{"name": i, "id": i} for i in income_high.columns],
-                    data=income_high.to_dict('records'),
-                    style_data={
-                        'whiteSpace': 'normal',
-                        'height': 'auto'
-                        },
-                )
-
-            
-            ]),
-
-            html.Div(id='income-graph', style={'margin': '10px'}, children=[
-
-                dcc.Graph(figure=income_fig)
-            ]),
-
-    ]),
-
-    ]),
-    
-
-    html.Div(id='Expense', children=[
-        
-        html.H2('Expense Summary'),
-        # Add more components for section 4 here
-
-        html.Div(id='expense-2', style={'display':'flex'},children=[
-             
-            html.Div(id='expense-table-div', style={'margin': '10px'}, children=[
-
-                dash_table.DataTable(
-
-                    id='expense-table',
-                    columns=[{"name": i, "id": i} for i in expense_high.columns],
-                    data=expense_high.to_dict('records'),
-                    style_data={
-                        'whiteSpace': 'normal',
-                        'height': 'auto'
-                        },
-                )
-
-            
-            ]),
-
-            html.Div(id='expense-graph', style={'margin': '10px'}, children=[
-
-                dcc.Graph(figure=expense_fig)
-            ]),
-
-    ]),
-
-    ]),
-
-    html.Div(id='debts', children=[
-        
-        html.H2('Debts'),
-
-        html.Div(id='debts-2', style={'display':'flex'},children=[
-
-            html.Div(id='loaned', style={'margin': '30px'}, children=[
-            
-            html.H3('Loaned Money'),
-            
-            current_debt[1]
-            # Add a div for diplaying networth here
 
         ]),
 
-        html.Div(id='owed', style={'margin': '30px'}, children=[
+        html.Div(id='Income', children=[
             
-            html.H3('Owed Money'),
-            
-            current_debt[2]
-            # Add a div for diplaying networth here
+            html.H2('Income Summary'),
+            # Add more components for section 4 here
+
+            html.Div(id='income-2', style={'display':'flex'},children=[
+                
+                html.Div(id='income-table-div', style={'margin': '10px'}, children=[
+
+                    dash_table.DataTable(
+
+                        id='income-table',
+                        columns=[{"name": i, "id": i} for i in income_high.columns],
+                        data=income_high.to_dict('records'),
+                        style_data={
+                            'whiteSpace': 'normal',
+                            'height': 'auto'
+                            },
+                    )
+
+                
+                ]),
+
+                html.Div(id='income-graph', style={'margin': '10px'}, children=[
+
+                    dcc.Graph(figure=income_fig)
+                ]),
 
         ]),
 
         ]),
-    ]),
+        
 
-    html.Div(id='investments', children=[
-        html.H2('Investments'),
-        # Add more components for section 7 here
-    ]),
-])
+        html.Div(id='Expense', children=[
+            
+            html.H2('Expense Summary'),
+            # Add more components for section 4 here
+
+            html.Div(id='expense-2', style={'display':'flex'},children=[
+                
+                html.Div(id='expense-table-div', style={'margin': '10px'}, children=[
+
+                    dash_table.DataTable(
+
+                        id='expense-table',
+                        columns=[{"name": i, "id": i} for i in expense_high.columns],
+                        data=expense_high.to_dict('records'),
+                        style_data={
+                            'whiteSpace': 'normal',
+                            'height': 'auto'
+                            },
+                    )
+
+                
+                ]),
+
+                html.Div(id='expense-graph', style={'margin': '10px'}, children=[
+
+                    dcc.Graph(figure=expense_fig)
+                ]),
+
+        ]),
+
+        ]),
+
+        html.Div(id='debts', children=[
+            
+            html.H2('Debts'),
+
+            html.Div(id='debts-2', style={'display':'flex'},children=[
+
+                html.Div(id='loaned', style={'margin': '30px'}, children=[
+                
+                html.H3('Loaned Money'),
+                
+                current_debt[1]
+                # Add a div for diplaying networth here
+
+            ]),
+
+            html.Div(id='owed', style={'margin': '30px'}, children=[
+                
+                html.H3('Owed Money'),
+                
+                current_debt[2]
+                # Add a div for diplaying networth here
+
+            ]),
+
+            ]),
+        ]),
+
+        html.Div(id='investments', children=[
+            html.H2('Investments'),
+            # Add more components for section 7 here
+        ]), 
+    ])
+    return layout
+
+app.layout = serve_layout
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0')
+    app.run_server(debug=True)
